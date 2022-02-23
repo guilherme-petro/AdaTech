08c0da809755025aa6f490320b87be442e13d7c5
guilherme-petro <guilhermespetro@gmail.com>
2022-02-22 23:03:58 -0300

    Adicionando os primeiros arquivos do projeto Spring

---
+++ adatech/src/main/java/com/generation/adatech/AdaTechApplication.java
@@ -0,0 +1,13 @@
+package com.generation.adatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaTechApplication.class, args);
+	}
+
+}
